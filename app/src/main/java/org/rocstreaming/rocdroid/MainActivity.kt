7825a5ff8c7ea31ec243143ca368dffd1c98455e
Valery <v.khomyakova.net@gmail.com>
2023-01-21 22:08:46 +0400

    Redesign =)

--- app/src/main/java/org/rocstreaming/rocdroid/MainActivity.kt
+++ app/src/main/java/org/rocstreaming/rocdroid/MainActivity.kt
@@ -1,50 +1,40 @@
 package org.rocstreaming.rocdroid
 
-import android.Manifest
 import android.content.ComponentName
 import android.content.Intent
 import android.content.ServiceConnection
 import android.content.SharedPreferences
-import android.content.pm.PackageManager
+import android.graphics.Color
 import android.media.AudioManager
-import android.media.projection.MediaProjection
-import android.media.projection.MediaProjectionManager
-import android.os.Build
 import android.os.Bundle
 import android.os.IBinder
-import android.text.Html
-import android.view.View
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
-import android.widget.TextView
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import java.net.NetworkInterface
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabItem
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import org.rocstreaming.rocdroid.adapter.ViewPagerAdapter
+import org.rocstreaming.rocdroid.fragment.ReceiverFragment
+import org.rocstreaming.rocdroid.fragment.SenderFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
-    private lateinit var receiverIpEdit: EditText
-    private lateinit var usePlaybackCapture: CheckBox
-    private lateinit var senderReceiverService: SenderReceiverService
-    private lateinit var projectionLauncher: ActivityResultLauncher<Intent>
-    private lateinit var projection: MediaProjection
-    private lateinit var projectionManager: MediaProjectionManager
+    private lateinit var pager: ViewPager2
+    private lateinit var tabs: TabLayout
+    private val senderFragment = SenderFragment()
+    private val receiverFragment = ReceiverFragment()
+
+    private lateinit var tabsTitle: Array<String>
+
     private lateinit var prefs: SharedPreferences
+    private lateinit var senderReceiverService: SenderReceiverService
 
     private val senderReceiverServiceConnection = object : ServiceConnection {
-
         override fun onServiceConnected(componentName: ComponentName, binder: IBinder) {
             senderReceiverService = (binder as SenderReceiverService.LocalBinder).getService()
-            senderReceiverService.setStateChangedListeners(
-                senderChanged = { state: Boolean -> runOnUiThread { setSenderButtonState(state) } },
-                receiverChanged = { state: Boolean -> runOnUiThread { setReceiverButtonState(state) } }
-            )
+
+            senderFragment.onServiceConnected(senderReceiverService, ::showActiveIcon, ::hideActiveIcon)
+            receiverFragment.onServiceConnected(senderReceiverService, ::showActiveIcon, ::hideActiveIcon)
         }
 
         override fun onServiceDisconnected(componentName: ComponentName) {
@@ -52,46 +42,49 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun showActiveIcon(tabIdx: Int) {
+        tabs.getTabAt(tabIdx)?.icon?.alpha = 255
+    }
+
+    fun hideActiveIcon(tabIdx: Int) {
+        tabs.getTabAt(tabIdx)?.icon?.alpha = 0
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
         volumeControlStream = AudioManager.STREAM_MUSIC
 
-        findViewById<TextView>(R.id.receiverExplanation).text =
-            Html.fromHtml(getText(R.string.receiver_expl).toString().format(getIpAddresses()), 0)
+        pager = findViewById(R.id.viewPager)
+        tabs = findViewById(R.id.tabs)
+
+        tabsTitle = arrayOf(
+            getText(R.string.receiver).toString(),
+            getText(R.string.sender).toString()
+        )
+
+        val adapter = ViewPagerAdapter(
+            getSupportFragmentManager(),
+            getLifecycle()
+        ).setFragments(
+            receiverFragment,
+            senderFragment
+        )
+
+        pager.setAdapter(adapter)
+
+        TabLayoutMediator(tabs, pager) { tab, position ->
+            tab.text = tabsTitle[position]
+            tab.icon = resources.getDrawable(R.drawable.round_indicator)
+            tab.icon?.alpha = 0
+            pager.setCurrentItem(tab.position, true)
+        }.attach()
 
         prefs = getSharedPreferences("settings", android.content.Context.MODE_PRIVATE)
-        receiverIpEdit = findViewById(R.id.receiverIp)
-        receiverIpEdit.setText(prefs.getString("receiver_ip", getText(R.string.default_receiver_ip).toString()))
-
-        usePlaybackCapture = findViewById(R.id.usePlaybackCapture)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            usePlaybackCapture.isEnabled = true
-            usePlaybackCapture.isChecked = prefs.getBoolean("playback_capture", true)
-        }
-
-        requestPermissionLauncher =
-            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
-                if (isGranted) {
-                    AlertDialog.Builder(this@MainActivity).apply {
-                        setTitle(R.string.allow_mic_title)
-                        setMessage(getString(R.string.allow_mic_ok_message))
-                        setCancelable(false)
-                        setPositiveButton(R.string.ok) { _, _ -> startStopSender(null) }
-                    }.show()
-                }
-            }
 
         val serviceIntent = Intent(this, SenderReceiverService::class.java)
         bindService(serviceIntent, senderReceiverServiceConnection, BIND_AUTO_CREATE)
-
-        projectionLauncher = registerForActivityResult(StartActivityForResult()) { result ->
-            if (result.data != null) {
-                projection = projectionManager.getMediaProjection(result.resultCode, result.data!!)
-                val ip = receiverIpEdit.text.toString()
-                senderReceiverService.startSender(ip, projection)
-            }
-        }
     }
 
     override fun onResume() {
@@ -102,103 +95,5 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         unbindService(senderReceiverServiceConnection)
-    }
-
-    /**
-     * Start roc receiver in separated thread and play samples via audioTrack
-     */
-    fun startStopReceiver(@Suppress("UNUSED_PARAMETER") view: View) {
-        if (senderReceiverService.isReceiverAlive()) {
-            senderReceiverService.stopReceiver()
-        } else {
-            senderReceiverService.startReceiver()
-        }
-    }
-
-    fun startStopSender(@Suppress("UNUSED_PARAMETER") view: View?) {
-        if (senderReceiverService.isSenderAlive()) {
-            senderReceiverService.stopSender()
-        } else {
-            val editor = prefs.edit()
-            editor.putBoolean("playback_capture", usePlaybackCapture.isChecked)
-            editor.putString("receiver_ip", receiverIpEdit.text.toString())
-            editor.apply()
-
-            if (!askForRecordAudioPermission()) return
-
-            if (usePlaybackCapture.isChecked) {
-                senderReceiverService.preStartSender()
-                projectionManager = getSystemService(MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
-                val projectionIntent = projectionManager.createScreenCaptureIntent()
-                startForegroundService(projectionIntent)
-                projectionLauncher.launch(projectionIntent)
-            } else {
-                val ip = receiverIpEdit.text.toString()
-                senderReceiverService.startSender(ip, null)
-            }
-        }
-    }
-
-    private fun askForRecordAudioPermission(): Boolean = when {
-        ContextCompat.checkSelfPermission(
-            this@MainActivity,
-            Manifest.permission.RECORD_AUDIO
-        ) == PackageManager.PERMISSION_GRANTED -> {
-            // Permission already granted.
-            true
-        }
-        shouldShowRequestPermissionRationale(Manifest.permission.RECORD_AUDIO) -> {
-            AlertDialog.Builder(this@MainActivity).apply {
-                setTitle(getString(R.string.allow_mic_title))
-                setMessage(getString(R.string.allow_mic_message))
-                setCancelable(false)
-                setPositiveButton(R.string.ok) { _, _ -> }
-            }.show()
-            false
-        }
-        else -> {
-            requestPermissionLauncher.launch(Manifest.permission.RECORD_AUDIO)
-            false
-        }
-    }
-
-    private fun getIpAddresses(): String {
-        try {
-            return NetworkInterface.getNetworkInterfaces().toList()
-                .flatMap { it.inetAddresses.toList() }
-                .filter { !it.isLoopbackAddress && !it.hostAddress.contains(':') }
-                .joinToString("\n") { it.hostAddress }
-        } catch (ignored: Exception) {
-        }
-        return ""
-    }
-
-    private fun setButtonState(
-        isRunning: Boolean,
-        button: Int,
-        startString: Int,
-        stopString: Int
-    ): Boolean {
-        findViewById<Button>(button).text = getText(if (isRunning) stopString else startString)
-        return isRunning
-    }
-
-    private fun setReceiverButtonState(isRunning: Boolean) {
-        setButtonState(
-            isRunning,
-            R.id.startReceiver,
-            R.string.start_receiver,
-            R.string.stop_receiver
-        )
-    }
-
-    private fun setSenderButtonState(isRunning: Boolean) {
-        val alive = setButtonState(
-            isRunning,
-            R.id.startSender,
-            R.string.start_sender,
-            R.string.stop_sender
-        )
-        findViewById<EditText>(R.id.receiverIp).isEnabled = !alive
     }
 }
---
+++ app/src/main/java/org/rocstreaming/rocdroid/fragment/ReceiverFragment.kt
@@ -0,0 +1,88 @@
+
+package org.rocstreaming.rocdroid.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import org.rocstreaming.rocdroid.R
+import org.rocstreaming.rocdroid.SenderReceiverService
+import org.rocstreaming.rocdroid.component.CopyBlock
+import java.net.NetworkInterface
+
+class ReceiverFragment : Fragment() {
+
+    private lateinit var receiverService: SenderReceiverService
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.receiver_fragment, container, false)
+
+        view.findViewById<CopyBlock>(R.id.sourcePortValue)?.setText("10001")
+        view.findViewById<CopyBlock>(R.id.repairPortValue)?.setText("10002")
+
+        val ipAddressesContainer: LinearLayout = view.findViewById(R.id.IPAddresses)
+
+        getIpAddresses().forEach {
+            val copyBlock = CopyBlock(requireActivity())
+            copyBlock.setText(it)
+
+            ipAddressesContainer.addView(copyBlock)
+        }
+
+        view.findViewById<TextView>(R.id.portForSource).text =
+            getText(R.string.receiver_sender_port_for_source).toString().format(3)
+
+        view.findViewById<TextView>(R.id.portForRepair).text =
+            getText(R.string.receiver_sender_port_for_repair).toString().format(4)
+
+        view.findViewById<Button>(R.id.startReceiverButton).setOnClickListener {
+            startStopReceiver(view)
+        }
+
+        return view
+    }
+
+    fun startStopReceiver(@Suppress("UNUSED_PARAMETER") view: View) {
+        if (receiverService.isReceiverAlive()) {
+            receiverService.stopReceiver()
+        } else {
+            receiverService.startReceiver()
+        }
+    }
+
+    fun onServiceConnected(
+        service: SenderReceiverService,
+        showActiveIcon: (Int) -> Unit,
+        hideActiveIcon: (Int) -> Unit) {
+        receiverService = service
+
+        receiverService.setReceiverStateChangedListeners(
+            receiverChanged = { state: Boolean ->
+                activity?.runOnUiThread {
+                    activity?.findViewById<Button>(R.id.startReceiverButton)?.text =
+                        getText(if (state) R.string.stop_receiver else R.string.start_receiver)
+                    if (state) showActiveIcon(0) else hideActiveIcon(0)
+                }
+            }
+        )
+    }
+
+    private fun getIpAddresses(): List<String> {
+        try {
+            return NetworkInterface.getNetworkInterfaces().toList()
+                .flatMap { it.inetAddresses.toList() }
+                .filter { !it.isLoopbackAddress && !it.hostAddress.contains(':') }
+                .map { it.hostAddress }
+        } catch (ignored: Exception) {
+        }
+        return emptyList()
+    }
+}
