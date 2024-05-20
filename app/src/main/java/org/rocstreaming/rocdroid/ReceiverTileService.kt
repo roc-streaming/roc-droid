3adf5df7637cc8d8934841dd9f01b06052ac16db
LinaLinn <lina.cloud@outlook.de>
2024-05-20 03:10:05 +0200

    Switch to not active tile and clean up ReceiverTileService

--- app/src/main/java/org/rocstreaming/rocdroid/ReceiverTileService.kt
+++ app/src/main/java/org/rocstreaming/rocdroid/ReceiverTileService.kt
@@ -14,18 +14,12 @@ import android.widget.Toast
 
 class ReceiverTileService : TileService() {
 
-    private var senderReceiverService: SenderReceiverService? = null;
+    private var senderReceiverService: SenderReceiverService? = null
 
     private val senderReceiverServiceConnection = object : ServiceConnection {
         override fun onServiceConnected(componentName: ComponentName, binder: IBinder) {
             val binder = binder as SenderReceiverService.LocalBinder
             senderReceiverService = binder.getService()
-
-            if ( senderReceiverService!!.isReceiverAlive() ) {
-                senderReceiverService!!.stopReceiver()
-            } else {
-                senderReceiverService!!.startReceiver()
-            }
         }
 
         override fun onServiceDisconnected(componentName: ComponentName) {
@@ -36,102 +30,37 @@ class ReceiverTileService : TileService() {
 
 
     override fun onCreate() {
-        Log.i("ROC-TileService", "onCreate: called")
 
-        //Toast.makeText(this.applicationContext, "onCreate", Toast.LENGTH_SHORT).show()
-        super.onCreate()
-    }
+        var intent = Intent(this.baseContext, SenderReceiverService::class.java)
+        this.applicationContext.bindService(intent,senderReceiverServiceConnection, BIND_AUTO_CREATE)
 
-    override fun onStart(intent: Intent?, startId: Int) {
-        Log.i("ROC-TileService", "onStart: called")
-
-        super.onStart(intent, startId)
-    }
-
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.i("ROC-TileService", "onStartCommand: called")
-
-        //Toast.makeText(this.applicationContext, "onStartCommand", Toast.LENGTH_SHORT).show()
-        return super.onStartCommand(intent, flags, startId)
-    }
-
-    override fun onDestroy() {
-        Log.i("ROC-TileService", "onDestroy: called")
-
-        //Toast.makeText(this.applicationContext, "onDestroy", Toast.LENGTH_SHORT).show()
-        super.onDestroy()
-    }
-
-    override fun onUnbind(intent: Intent?): Boolean {
-        Log.i("ROC-TileService", "onUnbind: called")
-
-        Toast.makeText(this.applicationContext, "onUnbind", Toast.LENGTH_SHORT).show()
-        return super.onUnbind(intent)
-    }
-
-    override fun onRebind(intent: Intent?) {
-        Log.i("ROC-TileService", "onRebind: called")
-
-        //Toast.makeText(this.applicationContext, "onRebind", Toast.LENGTH_SHORT).show()
-        super.onRebind(intent)
-    }
-
-    override fun onTileAdded() {
-        Log.i("ROC-TileService", "onTileAdded: called")
-
-        //Toast.makeText(this.applicationContext, "onTileAdded", Toast.LENGTH_SHORT).show()
-        super.onTileAdded()
-    }
-
-    override fun onTileRemoved() {
-        Log.i("ROC-TileService", "onTileRemoved: called")
-
-        //Toast.makeText(this.applicationContext, "onTileRemoved", Toast.LENGTH_SHORT).show()
-        super.onTileRemoved()
     }
 
     override fun onStartListening() {
-        Log.i("ROC-TileService", "onStartListening: called")
 
-        //Toast.makeText(this.applicationContext, "onStartListening", Toast.LENGTH_SHORT).show()
-        super.onStartListening()
-    }
+        if ( senderReceiverService!!.isReceiverAlive() ) {
+            this.qsTile.state =  Tile.STATE_ACTIVE
+        } else {
+            this.qsTile.state =  Tile.STATE_INACTIVE
+        }
 
-    override fun onStopListening() {
-        Log.i("ROC-TileService", "onStopListening: called")
-        //Toast.makeText(this.applicationContext, "onStopListening", Toast.LENGTH_SHORT).show()
-        super.onStopListening()
+        this.qsTile.updateTile()
     }
 
     override fun onClick() {
-        Log.i("ROC-TileService", "onClick: click")
-        Toast.makeText(this.applicationContext, "onClick", Toast.LENGTH_SHORT).show()
-        super.onClick()
 
-        val tile = this.qsTile
+        if (senderReceiverService != null) {
+            if ( senderReceiverService!!.isReceiverAlive() ) {
+                senderReceiverService!!.stopReceiver()
+                this.qsTile.state =  Tile.STATE_INACTIVE
+            } else {
+                senderReceiverService!!.startReceiver()
+                this.qsTile.state =  Tile.STATE_ACTIVE
 
-        var intent = Intent(this.baseContext, SenderReceiverService::class.java)
-
-        var serviceBinded = this.applicationContext.bindService(intent,senderReceiverServiceConnection,
-            BIND_AUTO_CREATE
-        )
-
-        if (serviceBinded) {
-            if (senderReceiverService != null) {
-                if ( senderReceiverService!!.isReceiverAlive() ) {
-                    senderReceiverService!!.stopReceiver()
-                    tile.state =  Tile.STATE_INACTIVE
-                } else {
-                    senderReceiverService!!.startReceiver()
-                    tile.state =  Tile.STATE_ACTIVE
-
-                }
             }
         }
 
-
-
-        tile.updateTile()
+        this.qsTile.updateTile()
 
     }
 }
