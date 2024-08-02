62ac86ded6e203efb9c1a755989fbb2b2ed0109b
Victor Gaydov <victor@enise.org>
2024-08-02 23:08:18 +0400

    Update dodo.py (#110)
    - remove __pycache__ at exit
    - don't print backtrace on ^C
    - add commands:
        doit analyze
        doit test
        doit exec
        doit build_apk
        doit wipe
    - use LongRunning()
    - add more doit files to .gitignore
    - use doit on CI
    - separate CI jobs for desktop & android

--- dodo.py
+++ dodo.py
@@ -1,4 +1,58 @@
-from doit.tools import title_with_actions
+from doit.tools import title_with_actions, LongRunning
+import atexit
+import os
+import shutil
+import signal
+
+atexit.register(
+    lambda: shutil.rmtree('__pycache__', ignore_errors=True))
+if os.name == 'posix':
+    signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
+
+DOIT_CONFIG = {
+    'default_tasks': ['analyze', 'test'],
+    'verbosity': 2,
+}
+
+# doit analyze
+def task_analyze():
+    """run analyzer"""
+    return {
+        'actions': ['flutter analyze --fatal-infos --fatal-warnings'],
+        'title': title_with_actions,
+    }
+
+# doit test
+def task_test():
+    """run tests"""
+    return {
+        'actions': ['flutter test -j1 -r github'],
+        'title': title_with_actions,
+    }
+
+# doit exec
+def task_exec():
+    """run on device"""
+    return {
+        'actions': [LongRunning('flutter run --release')],
+        'title': title_with_actions,
+    }
+
+# doit build_apk
+def task_build_apk():
+    """build android apk"""
+    return {
+        'actions': ['flutter build apk --release'],
+        'title': title_with_actions,
+    }
+
+# doit wipe
+def task_wipe():
+    """clean build artifacts"""
+    return {
+        'actions': ['flutter clean'],
+        'title': title_with_actions,
+    }
 
 # doit gen
 def task_gen():
@@ -6,16 +60,14 @@ def task_gen():
     return {
         'actions': ['dart run build_runner build --delete-conflicting-outputs'],
         'title': title_with_actions,
-        'verbosity': 2,
     }
 
 # doit gen_watch
 def task_gen_watch():
     """run flutter build_runner code watch generation"""
     return {
-        'actions': ['dart run build_runner watch --delete-conflicting-outputs'],
+        'actions': [LongRunning('dart run build_runner watch --delete-conflicting-outputs')],
         'title': title_with_actions,
-        'verbosity': 2,
     }
 
 # doit icons
@@ -24,7 +76,6 @@ def task_icons():
     return {
         'actions': ['dart run flutter_launcher_icons'],
         'title': title_with_actions,
-        'verbosity': 2,
     }
 
 # doit splash
@@ -33,5 +84,4 @@ def task_splash():
     return {
         'actions': ['dart run flutter_native_splash:create '],
         'title': title_with_actions,
-        'verbosity': 2,
     }
