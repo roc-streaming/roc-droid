f5f8b6e71e448cb16f8469570b470eabe07cb5d4
Victor Gaydov <victor@enise.org>
2024-10-05 21:57:40 +0700

    Build system update (#104)
    - Get minSdk, targetSdk, compileSdk from gradle.properties
    - Get version and version code from AndroidManifest
    - Check AndroidManifest and pubspec versions when building APK
    - Run spotless linter and dart analyzer
    - Run spotless formatter and dart formatter
    - Restore APK signing config

--- dodo.py
+++ dodo.py
@@ -1,13 +1,18 @@
 from doit import get_var
 from doit.tools import title_with_actions, LongRunning, Interactive
 import atexit
+import fnmatch
 import glob
 import os
+import pathlib
+import platform
 import platform
 import shutil
 import signal
 import subprocess
 import sys
+import xml.etree.ElementTree
+import yaml
 
 atexit.register(
     lambda: shutil.rmtree('__pycache__', ignore_errors=True))
@@ -15,14 +20,36 @@ if os.name == 'posix':
     signal.signal(signal.SIGINT, lambda s, f: exit(1))
 
 DOIT_CONFIG = {
-    'default_tasks': ['analyze', 'test'],
+    'default_tasks': ['lint', 'test'],
     'verbosity': 2,
 }
 
 VARIANT = get_var('variant', 'release')
 WATCH = get_var('watch', 'false')
 
-# Delete all files matching glob pattern.
+def _pubspec_version():
+    with open('pubspec.yaml') as fp:
+        spec = yaml.safe_load(fp)
+        return spec['version']
+
+def _android_version():
+    with open('android/app/src/main/AndroidManifest.xml') as fp:
+        root = xml.etree.ElementTree.fromstring(fp.read())
+        return root.attrib['{http://schemas.android.com/apk/res/android}versionName']
+
+def _gradlew():
+    if platform.system() == 'Windows':
+        return 'gradlew.bat'
+    else:
+        return 'gradlew'
+
+def _copy_file(src, dst):
+    def task():
+        print(f'\nCopying {src}\n     to {dst}', file=sys.stderr)
+        os.makedirs(os.path.dirname(dst), exist_ok=True)
+        shutil.copy2(src, dst)
+    return task
+
 def _delete_files(pattern):
     def task():
         for path in glob.glob(pattern):
@@ -33,18 +60,37 @@ def _delete_files(pattern):
                 os.remove(path)
     return task
 
-# doit analyze
-def task_analyze():
+# doit lint
+def task_lint():
+    """run all linters"""
+    return {
+        'basename': 'lint',
+        'actions': None,
+        'task_dep': ['lint:dart', 'lint:kotlin'],
+    }
+
+# doit lint:dart
+def task_lint_dart():
     """run dart analyzer"""
     return {
+        'basename': 'lint:dart',
         'actions': ['flutter analyze --fatal-infos --fatal-warnings'],
         'title': title_with_actions,
+    }
+
+# doit lint:kotlin
+def task_lint_kotlin():
+    """run spotless linter"""
+    return {
+        'basename': 'lint:kotlin',
+        'actions': [f'cd android && ./{_gradlew()} spotlessCheck'],
     }
 
 # doit test
 def task_test():
     """run tests"""
     return {
+        'basename': 'test',
         'actions': ['flutter test -j1 -r expanded'],
         'title': title_with_actions,
     }
@@ -52,15 +98,16 @@ def task_test():
 # doit launch [variant=debug|release]
 def task_launch():
     """deploy and run on device"""
-    def do_cleanup():
+    def _do_cleanup():
         subprocess.call(
             'adb shell am force-stop org.rocstreaming.rocdroid',
             shell=True)
-    def register_cleanup():
-        atexit.register(do_cleanup)
+    def _register_cleanup():
+        atexit.register(_do_cleanup)
     return {
+        'basename': 'launch',
         'actions': [
-            register_cleanup,
+            _register_cleanup,
             Interactive(f'flutter run --{VARIANT}'),
         ],
         'title': title_with_actions,
@@ -80,7 +127,13 @@ def task_build_apk():
     """build android apk"""
     return {
         'basename': 'build:apk',
-        'actions': [f'flutter build apk --{VARIANT}'],
+        'actions': [
+            f'flutter build apk --{VARIANT}',
+            _copy_file(
+                f'build/app/outputs/flutter-apk/app-{VARIANT}.apk',
+                f'dist/android/{VARIANT}/roc-droid-{_android_version()}.apk'
+            ),
+        ],
         'title': title_with_actions,
     }
 
@@ -88,9 +141,11 @@ def task_build_apk():
 def task_wipe():
     """remove all build artifacts"""
     return {
+        'basename': 'wipe',
         'actions': [
             'flutter clean',
             _delete_files('android/.gradle'),
+            _delete_files('android/build'),
         ],
         'title': title_with_actions,
     }
@@ -142,4 +197,37 @@ def task_gen_splash():
         'basename': 'gen:splash',
         'actions': ['dart run flutter_native_splash:create '],
         'title': title_with_actions,
+    }
+
+# doit fmt
+def task_fmt():
+    """run all code formatters"""
+    return {
+        'basename': 'fmt',
+        'actions': None,
+        'task_dep': ['fmt:dart', 'fmt:kotlin'],
+    }
+
+# doit fmt:dart
+def task_fmt_dart():
+    """run dart code formatter"""
+    def _is_ignored(path):
+        if fnmatch.fnmatch(path.name, '*.g.dart'):
+            return True
+        return False
+    files = list(sorted(
+        map(str,
+            filter(lambda x: not _is_ignored(x),
+                   pathlib.Path('lib').rglob('*.dart')))))
+    return {
+        'basename': 'fmt:dart',
+        'actions': [['dart', 'format', *files]],
+    }
+
+# doit fmt:kotlin
+def task_fmt_kotlin():
+    """run spotless formatter"""
+    return {
+        'basename': 'fmt:kotlin',
+        'actions': [f'cd android && ./{_gradlew()} spotlessApply'],
     }
