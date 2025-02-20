55b7f3d2e56768025cf3324076ef1d33ff2e38fe
Victor Gaydov <victor@enise.org>
2025-02-21 00:39:38 +0900

    Enable more linter rules
    Some are cosmetic, however a few are important:
    
    1. await_only_futures - detect use of `await` where it has no effect
    2. discarded_futures - detect missing `await` when calling `async` func
    3. depend_on_referenced_packages - detect imported packages missing
       from pubspec
    
    These 3 rules found a few bugs:
    
    - unneeded await for xxxIsAlive (this commit removes it)
    
    - missing await for _setDefultValues (this commit temporary
      adds `ignore` comment, this should be fixed separately)
    
    - invalid dependency: in pubspec we had `flutter_localization`,
      but in code we used `flutter_localizations` (note the "s"),
      they are different packages
      (this commit fixes pubspec)

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_localization
-import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterLocalizationPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalizationPlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <flutter_localization/flutter_localization_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FlutterLocalizationPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterLocalizationPluginCApi"));
 }
